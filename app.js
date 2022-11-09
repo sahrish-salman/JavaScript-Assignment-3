cf26d7f03f7fb3092214280de43593f913f1acf4
sahrish-salman <sahrish.salman@gmail.com>
2022-11-09 17:12:25 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,130 @@
+// var city0 = "Atlanta";
+// var city1 = "Baltimore";
+// var city2 = "Chicago";
+// var city3 = "Denver";
+// var city4 = "Los Angeles";
+// var city5 = "Seattle";
+
+// alert("Welcome to " + city3);
+
+// var cities = ["Atlanta", "Baltimore", "Chicago", "Denver", "Los Angeles", "Seattle"];
+
+// alert("Welcome to " + cities[0]);
+
+// var mixedArray = [1, "Bob", "Now is", "true"];
+
+// alert(mixedArray[3]);
+
+// var pets= ["dog", "cat", "bird", "lizard", "fish", "gerbil", "snake" ]
+
+// console.log(pets[4]);
+
+// var pets= ["dog", "cat", "bird", "lizard", "fish", "gerbil", "snake" ]
+
+// pets.pop();
+// console.log(pets);
+
+// var pets= ["dog", "cat", "bird", "lizard", "fish", "gerbil", "snake" ]
+
+// pets.push("fish", "ferret");
+// console.log(pets);
+
+// var pets= ["dog", "cat", "bird"]
+
+// pets.shift();
+// console.log(pets);
+
+// var pets= ["dog", "cat", "bird"]
+
+// pets.unshift("fish", "ferret");
+// console.log(pets);
+
+// var pets = ["dog", "cat", "fly", "bug", "ox"]
+
+// pets.splice(2, 2, "cock", "duck", "emu");
+// console.log(pets);
+
+// var pets = ["dog", "cat", "fly", "bug", "ox"]
+
+// pets.splice(2, 0, "cock", "duck", "emu");
+// console.log(pets);
+
+// var pets = ["dog", "cat", "fly", "bug", "ox"]
+
+// pets.splice(2, 2);
+// console.log(pets);
+
+// var pets = ["dog", "cat", "fly", "bug", "ox"]
+
+// var nopets = pets.slice(2, 4);
+// console.log(nopets);
+
+// var pets = ["dog", "cat", "fly", "bug", "ox"]
+
+// var nopets = pets.slice(0, 4);
+// console.log(nopets);
+
+// for (var i = 0; i < 50; i++) {
+//     console.log(i);
+// }
+
+// for (var i = 0; i < 50; i++) {
+//     console.log(i +1)
+// }
+
+// var cleanestCities = ["Cheyenne", "Santa Fe", "Tucson", "Great Falls", "Honolulu"];
+// var cityToCheck = prompt("type your city name to check");
+// for (var i = 0; i <= 4; i++)
+
+// if (cityToCheck === cleanestCities[i]) {
+// alert("It's one of the cleanest cities");
+// }
+
+
+// var cleanestCities = ["Cheyenne", "Santa Fe", "Tucson", "Great Falls", "Honolulu"];
+// var cityToCheck = prompt("type your city name to check");
+// var matchFound = "no";
+// for (var i = 0; i <= 4; i++);
+
+// if (cityToCheck === cleanestCities[i]) {
+// matchFound = "yes";
+// alert("It's one of the cleanest cities");
+// }
+// if (matchFound === "no") {
+// alert("It's not on the list");
+// }
+
+// var matchFound = false;
+// for (var i = 0; i <= 4; i++);
+// if (cityToCheck === cleanestCities[i]) {
+// matchFound = true;
+// alert("It's one of the cleanest cities");
+// }
+// }
+// if (matchFound === false) {
+// alert("It's not on the list");
+// }
+
+// var cleanestCities = ["Cheyenne", "Santa Fe", "Tucson", "Great Falls", "Honolulu"];
+// var cityToCheck = prompt("type your city name to check");
+// var numElements = cleanestCities.length;
+// var matchFound = false;
+
+// for (var i = 0; i < numElements; i++);
+// if (cityToCheck === cleanestCities[i]) {
+// matchFound = true;
+// alert("It's one of the cleanest cities");
+// } 
+// if (matchFound === false) {
+// alert("It's not on the list");
+// }
+
+// var firstNames = ["BlueRay ", "Upchuck ", "Lojack ", "Gizmo ", "Do-Rag "];
+// var lastNames = ["Zzz", "Burp", "Dogbone", "Droop"];
+// var fullNames = [];
+// for (var i = 0; i < firstNames.length; i++) {
+// for (var j = 0; j < lastNames.length; j++) {
+// fullNames.push(firstNames[i] + lastNames[j]);
+// }
+// }
+// console.log(fullNames);
